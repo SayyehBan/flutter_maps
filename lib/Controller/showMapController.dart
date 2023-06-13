8413e0b5e238f083bedf6fe443ad2ac0c701986e
SayyehBan <sdvp1991david@gmail.com>
2023-06-13 16:31:54 +0330

    مدیریت موقعیت لوکیشن در مبدا و مقصد و برگشت

--- lib/Controller/showMapController.dart
+++ lib/Controller/showMapController.dart
@@ -2,6 +2,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_maps/components/buttonSelect.dart';
+import 'package:flutter_maps/gen/assets.gen.dart';
+import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
 import 'package:get/get.dart';
 
 enum CurrentWidgetStates {
@@ -11,15 +13,43 @@ enum CurrentWidgetStates {
 }
 
 // Create a class to manage the widget state
-class OperationButtonController extends GetxController {
+class ShowMapController extends GetxController {
   // Define an observable variable to store the current widget state
   final currentWidgetLists = RxList<CurrentWidgetStates>([
     CurrentWidgetStates.stateSelectOrigin,
   ]);
+  MapController mapController = MapController(
+      initPosition: GeoPoint(latitude: 35.652903, longitude: 51.059940));
+  final geoPoints = RxList<GeoPoint>(<GeoPoint>[]);
+  RxString markerIcon = Assets.icons.origin.obs;
 
   // Define a function to update the widget state when the user selects a destination
-  void selectDestination() {
+  void selectDestination() async {
+    GeoPoint originGeoPoint =
+        await mapController.getCurrentPositionAdvancedPositionPicker();
+    debugPrint(
+        "latitude : ${originGeoPoint.latitude} longitude : ${originGeoPoint.longitude}");
+    geoPoints.add(originGeoPoint);
+
     currentWidgetLists.add(CurrentWidgetStates.stateSelectDestination);
+    markerIcon.value = Assets.icons.destination;
+
+    mapController.init();
+  }
+
+  void backButton() {
+    if (geoPoints.isNotEmpty) {
+      geoPoints.removeLast();
+      markerIcon.value = Assets.icons.origin;
+      mapController.init();
+    }
+    if (currentWidgetLists.length > 1) {
+      currentWidgetLists.removeLast();
+    }
+    if (currentWidgetLists.length == 1) {
+      markerIcon.value = Assets.icons.origin;
+      mapController.init();
+    }
   }
 
   // Define a function to update the widget state when the user requests a driver
--- lib/view/mapScreen/mapScreen.dart
+++ lib/view/mapScreen/mapScreen.dart
@@ -1,20 +1,18 @@
 // ignore_for_file: file_names, must_be_immutable
 
 import 'package:flutter/material.dart';
-import 'package:flutter_maps/Controller/mapController.dart';
-import 'package:flutter_maps/Controller/operationButtonController.dart';
+import 'package:flutter_maps/Controller/showMapController.dart';
 import 'package:flutter_maps/components/getBackPage.dart';
+import 'package:flutter_maps/gen/assets.gen.dart';
 import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 
 class MapScreen extends StatelessWidget {
   MapScreen({super.key});
-  final operationButtonController = Get.put(OperationButtonController());
   final showMapController = Get.put(ShowMapController());
-  MapController mapController = MapController(
-      initPosition:
-          GeoPoint(latitude: 35.7367516373, longitude: 51.2911096718));
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -23,7 +21,7 @@ class MapScreen extends StatelessWidget {
         //OSM Ma
         SizedBox.expand(
           child: OSMFlutter(
-            controller: mapController,
+            controller: showMapController.mapController,
             userTrackingOption: const UserTrackingOption(
               enableTracking: false,
               unFollowUser: false,
@@ -38,20 +36,22 @@ class MapScreen extends StatelessWidget {
             stepZoom: 1,
             markerOption: MarkerOption(
                 advancedPickerMarker: MarkerIcon(
-              iconWidget: showMapController.markerIcon,
-            )),
+                    iconWidget: Obx(
+              () => SvgPicture.asset(
+                showMapController.markerIcon.value,
+                height: 100,
+                width: 40,
+              ),
+            ))),
           ),
         ),
 
-        Obx(() => operationButtonController.currentWidget),
+        Obx(() => showMapController.currentWidget),
         Obx(
-          () => operationButtonController.currentWidgetLists.length != 1
+          () => showMapController.currentWidgetLists.length != 1
               ? GetBackPage(
                   onPressed: () {
-                    if (operationButtonController.currentWidgetLists.length >
-                        1) {
-                      operationButtonController.currentWidgetLists.removeLast();
-                    }
+                    showMapController.backButton();
                   },
                 )
               : const SizedBox(),
